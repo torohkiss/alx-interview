92b719f8500a2a32dd36fda0c46575e1b337feed
torohkiss <torohkiss@yahoo.com>
2024-10-21 22:57:00 +0000

    task 0

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+0-pascal_triangle
+"""
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+    triangle = []
+    for i in range(n):
+        row = []
+        for j in range(i + 1):
+            if j == 0 or j == i:
+                row.append(1)
+            else:
+                row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        triangle.append(row)
+    return triangle
