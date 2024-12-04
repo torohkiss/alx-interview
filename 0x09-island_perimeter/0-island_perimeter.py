f2e2871b60c3b590b33498518fb1a0f46fd95a6e
torohkiss <torohkiss@yahoo.com>
2024-12-04 17:30:52 +0000

    task 0

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""A module that calculates an Island Perimeter"""
+
+
+def island_perimeter(grid):
+    """The island perimeter function"""
+
+    perimeter = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == 1:
+                perimeter += 4
+
+                if i > 0 and grid[i-1][j] == 1:
+                    perimeter -= 1
+                if i < len(grid) - 1 and grid[i + 1][j] == 1:
+                    perimeter -= 1
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 1
+                if j < len(grid[0]) - 1 and grid[i][j + 1] == 1:
+                    perimeter -= 1
+    return perimeter
